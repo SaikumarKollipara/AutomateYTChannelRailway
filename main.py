a5ad9458980170b09fef52bb96a2e0b13471ad46
saikumar kollipara <krksaikumar@gmail.com>
2022-12-15 13:20:42 +0530

    starting notebook

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from extract import *
 import os
 
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+from time import sleep
+from datetime import timedelta
+
+
 SECRET = os.getenv("SECRET")
 
 #
@@ -16,11 +24,12 @@ class Msg(BaseModel):
 @app.get("/")
 
 async def root():
-    driver = createDriver()
-    driver.get('https://youtube.com')
-    source_code = driver.page_source
-    print(source_code[:100])
-    return {"message": "Hello World. Welcome to FastAPI!", "source_code": source_code}
+    # driver = createDriver()
+    # driver.get('https://youtube.com')
+    # source_code = driver.page_source
+    # print(source_code[:100])
+    run()
+    return {"message": "Hello World. Welcome to FastAPI!"}
 
 
 @app.get("/homepage")
@@ -39,3 +48,40 @@ async def demo_post(inp: Msg, background_tasks: BackgroundTasks):
     
 
 
+def run():
+    driver = createDriver()
+    driver.get('https://kaggle.com')
+    sleep(2)
+    print('browser opened')
+
+    #signIn button
+    driver.find_element_by_css_selector('#site-container > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(2) a').click()
+    sleep(2)
+    # email signin
+    driver.find_element_by_css_selector('form > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) a').click()
+    #email & pass
+    sleep(2)
+    email = driver.find_element_by_css_selector('[name="email"]')
+    email.send_keys('kkrksaikumar@gmail.com')
+    email.send_keys(Keys.ENTER)
+    password = driver.find_element_by_css_selector('[name="password"]')
+    password.send_keys('Skumar@123')
+    password.send_keys(Keys.ENTER)
+    sleep(2)
+
+    # Go to notebook
+    driver.get('https://www.kaggle.com/code/krksaikumar/automateytchannel/edit')
+    print('notebook opened')
+    sleep(35)
+
+    # run all 
+    driver.find_element_by_css_selector('[title="Run all"]').click()
+    print('running')
+
+    totalTime = (3 * 60 * 60) + (0 * 60) + (0)
+    logTime = (0 * 60 * 60) + (10 * 60) + (0)
+    for i in range(1, totalTime//logTime+1):
+        print(f'{(logTime * i)} {timedelta(seconds=logTime * i)} completed')
+        sleep(logTime)
+    
+    driver.close()
