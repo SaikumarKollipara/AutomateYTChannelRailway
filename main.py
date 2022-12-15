baee39612dd778b137c68620fef0f88c2ac17aad
saikumar kollipara <krksaikumar@gmail.com>
2022-12-15 18:19:01 +0530

    testing

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ async def root():
     # source_code = driver.page_source
     # print(source_code[:100])
     
-    source_code = test()
-    return {"message": "Hello World. Welcome to FastAPI!", 'source_code': source_code}
+    run()
+    return {"message": "Hello World. Welcome to FastAPI!"}
 
 
 @app.get("/homepage")
@@ -80,11 +80,11 @@ def run():
     driver.find_element_by_css_selector('[title="Run all"]').click()
     print('running')
 
-    totalTime = (3 * 60 * 60) + (0 * 60) + (0)
-    logTime = (0 * 60 * 60) + (10 * 60) + (0)
-    for i in range(1, totalTime//logTime+1):
-        print(f'{(logTime * i)} {timedelta(seconds=logTime * i)} completed')
-        sleep(logTime)
+    # totalTime = (3 * 60 * 60) + (0 * 60) + (0)
+    # logTime = (0 * 60 * 60) + (10 * 60) + (0)
+    # for i in range(1, totalTime//logTime+1):
+    #     print(f'{(logTime * i)} {timedelta(seconds=logTime * i)} completed')
+    #     sleep(logTime)
     
     driver.close()
     
